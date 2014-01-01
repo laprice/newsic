9e685a1132b48e15e6d750bd2b255dc32ca1f7f8
Larry Price <larry@industrialintellect.com>
2014-01-01 23:01:38 +0000

    starting in.

---
+++ test/test_fetch.py
@@ -0,0 +1,10 @@
+import unittest
+from news import Feed
+from news import Config
+
+class TestFeedFetchFromURL(unittest.TestCase):
+    def SetUp():
+        pass
+
+if __name__=='__main__':
+    unittest.main()
