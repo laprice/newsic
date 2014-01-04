53ec1c9bed5536dd9ebb59bf09d207d6ddea7d33
Larry Price <larry@industrialintellect.com>
2014-01-04 07:05:40 +0000

    got to make the push of the day.

--- test/test_fetch.py
+++ test/test_fetch.py
@@ -4,7 +4,11 @@ from news import Config
 
 class TestFeedFetchFromURL(unittest.TestCase):
     def SetUp():
-        pass
+        urls = ['http://news.ycombinator.com/rss']
+
+    def test_fetch(urls):
+        feeds = [ Feed(url) for url in urls ]
+        self.assertTrue(len(feeds) > 0)
 
 if __name__=='__main__':
     unittest.main()
