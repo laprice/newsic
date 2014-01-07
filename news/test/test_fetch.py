ff88391403a2dd89d29bbbff45d7ce97911c0253
Larry Price <larry@industrialintellect.com>
2014-01-07 05:09:40 +0000

    relative imports

---
+++ news/test/test_fetch.py
@@ -0,0 +1,12 @@
+import unittest
+import sys
+sys.path.append('..')
+from feeds import Feed
+
+
+class TestFeedFetchFromURL(unittest.TestCase):
+    def SetUp():
+        pass
+
+if __name__=='__main__':
+    unittest.main()
