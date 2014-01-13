7959cde3657c4fb8787ce27f881edd33ad17a8b1
Larry Price <larry@industrialintellect.com>
2014-01-13 06:39:51 +0000

    adding test for __str__

--- news/test/test_fetch.py
+++ news/test/test_fetch.py
@@ -7,11 +7,17 @@ from feeds import Feed
 class TestFeedFetchFromURL(unittest.TestCase):
     def setUp(self):
         self.feedlist = [ 'http://news.ycombinator.com/rss' ]
-    
+        self.f = [ Feed(feed) for feed in self.feedlist ]
+
     def test_feeds(self):
-        f = [ Feed(feed) for feed in self.feedlist ]
-        n = f.pop()
+        n = self.f[0]
         self.assertIsInstance(n,Feed)
+
+    def test_feed_repr(self):
+        n = self.f[0]
+        t = """title: http://news.ycombinator.com/rss
+source: http://news.ycombinator.com/rss"""
+        self.assertEquals(n.__str__(),t)
 
 if __name__=='__main__':
     unittest.main()
