33fffe6b4341ec27aed76d757d989378380fec7b
Larry Price <larry@industrialintellect.com>
2014-01-18 07:38:39 +0000

    okay this is broken. But I'm checking it in cause it's broken in a useful way.

--- news/views.py
+++ news/views.py
@@ -1,9 +1,17 @@
-import .feeds
-from os.path import exists
-if exists('~/.newsic/feeds.txt'):
-    feeds = open('~/.newsic/feeds.txt').read()
+import feeds
+import os.path as op
+import pprint
 
-bleah = [ Feed(url) for url in feeds.split('\n') ]
+if op.exists('~/.newsic/feeds.txt'):
+    print "found feeds.txt"
+    myfeeds = open('~/.newsic/feeds.txt').read().split('\n')
+else:
+    print "gah!!! no feeds.txt giving you stupid"
+    myfeeds = ['https://news.ycombinator.com/rss']
+
+bleah = [ feeds.Feed(url) for url in myfeeds ]
+counts = [ (f.title, f.fetch() ) for f in bleah ]
+
 
 for b in bleah:
     b.display()
