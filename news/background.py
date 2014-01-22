580fae5826221ca172755c56f02d374795c122fd
Larry Price <larry@industrialintellect.com>
2014-01-22 04:27:40 +0000

    yeah writing this drunk, no code; fuck you.

---
+++ news/background.py
@@ -0,0 +1,10 @@
+import feeds
+import requests
+#import word2vec
+
+#background worker
+# pulls in feeds
+# fetches link content
+# vectorizes
+# matches against my known preferences
+# attempts to summarize
